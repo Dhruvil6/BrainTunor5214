4ccf718858af93eab24441055a8f038014174220
personal <upLiftL1f3@gmail.com>
2024-12-07 16:06:10 -0600

    fixes

--- src/components/showRow.tsx
+++ src/components/showRow.tsx
@@ -1,31 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ImageGrid from "./content";
 
 // Removed `headers` since it was unused
 const ShowRow: React.FC = () => {
-    const [isDarkMode, setIsDarkMode] = useState(
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-    );
-    const [selectedValue, setSelectedValue] = useState<number>(1);
-
-    useEffect(() => {
-        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-        setIsDarkMode(mediaQuery.matches);
-
-        // Renamed to avoid conflict with the `handleChange` for `selectedValue`
-        const handleMediaChange = (e: MediaQueryListEvent) =>
-            setIsDarkMode(e.matches);
-        mediaQuery.addEventListener("change", handleMediaChange);
-
-        return () =>
-            mediaQuery.removeEventListener("change", handleMediaChange);
-    }, []);
-
-    const handleChange = (value: number) => setSelectedValue(value);
-
-    const getBorderColor = () => (isDarkMode ? "gray" : "black");
-
     return (
         <div>
             <h3>Select an image</h3>
